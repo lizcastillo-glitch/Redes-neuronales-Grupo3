5869378f0ef2210c064ad2c808eed582178fe29b
lizcastillo-glitch <liz.castillo@uees.edu.ec>
2025-12-17 22:00:14 -0500

    Update neural_network.py
    # Placeholder

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,3 +1,12 @@
 """
 Implementación desde cero de una red neuronal feedforward usando NumPy.
 """
+class NeuralNetwork:
+    def __init__(self, layers, activation='relu', init='xavier'):
+        ...
+    
+    def train(self, X, y, epochs, lr, batch_size=32, verbose=True):
+        ...
+    
+    def predict(self, X):
+        ...
